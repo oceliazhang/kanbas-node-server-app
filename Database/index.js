6a9054a959252023b1980d70ba03b6a921844ed1
oceliazhang <oceliaaa@163.com>
2023-11-20 19:27:25 -0800

    change syntax import->require

--- Database/index.js
+++ Database/index.js
@@ -1,4 +1,7 @@
-import courses from "./courses.json" assert { type: "json" };
+const courses = require("./courses.json");
+
+
+//import courses from "./courses.json" assert { type: "json" };
 import modules from "./modules.json" assert { type: "json" };
 import assignments from "./assignments.json" assert { type: "json" };
 // import users from "./users.json" assert { type: "json" };

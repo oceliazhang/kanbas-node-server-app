cef143330b89a17b759e0ab6f13ebf92550664c3
oceliazhang <oceliaaa@163.com>
2023-11-20 19:37:22 -0800

    set node version to 18

--- Database/index.js
+++ Database/index.js
@@ -1,11 +1,11 @@
-const courses = require("./courses.json");
-const modules = require("./modules.json");
-const assignments = require("./assignments.json");
+// const courses = require("./courses.json");
+// const modules = require("./modules.json");
+// const assignments = require("./assignments.json");
 
 
-//import courses from "./courses.json" assert { type: "json" };
-//import modules from "./modules.json" assert { type: "json" };
-//import assignments from "./assignments.json" assert { type: "json" };
+import courses from "./courses.json" assert { type: "json" };
+import modules from "./modules.json" assert { type: "json" };
+import assignments from "./assignments.json" assert { type: "json" };
 // import users from "./users.json" assert { type: "json" };
 // import grades from "./grades.json" assert { type: "json" };
 // import enrollments from "./enrollments.json" assert { type: "json" };

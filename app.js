6bfbfe0a49cd26823b88dedb23b0dcc9230fc5eb
oceliazhang <oceliaaa@163.com>
2023-11-20 18:50:13 -0800

    deploy env

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-// import express from "express";
-// import Hello from "./hello.js";
-// import Lab5 from "./lab5.js";
-// import cors from "cors";
-// import CourseRoutes from "./courses/routes.js";
-
-// const app = express();
-// app.use(cors());
-// app.use(express.json());
-// CourseRoutes(app);
-// Lab5(app);
-// Hello(app);
-// app.listen(4000);
-
-
-// const express = require("express");
-import "dotenv/config";
 import express from "express";
-import HelloRoutes from "./hello.js";
+import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
+import cors from "cors";
 import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
-import cors from "cors";
+import "dotenv/config";
+import session from "express-session";
+
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-ModuleRoutes(app);
-
-ModuleRoutes(app);
 CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
-HelloRoutes(app);
+Hello(app);
+//app.listen(4000);
 
 app.listen(process.env.PORT || 4000);
+
 
 

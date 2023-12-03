0670f914331a7dd5da5e3f635176b42f2f58805d
oceliazhang <oceliaaa@163.com>
2023-12-03 03:14:14 -0800

    frontend url

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(
     cors({
       credentials: true,
-      origin: process.env.FRONTEND_URL
+      origin: "https://a6--gilded-khapse-a03d2c.netlify.app/"
     })
    );
 

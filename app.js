7cd98eb2a7d34b17f0e52af726c8054446ac98a8
oceliazhang <oceliaaa@163.com>
2023-12-03 23:15:13 -0800

    cookie-session

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ import CourseRoutes from "./courses/routes.js";
 import ModuleRoutes from "./modules/routes.js";
 //import AssignmentRoutes from "./assignment/routes.js";
 import UserRoutes from "./users/routes.js";
-import session from "express-session";
+//import session from "express-session";
+import session from "cookie-session";
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/kanbas';
 //const CONNECTION_STRING = 'mongodb://127.0.0.1:27017/kanbas';
 
